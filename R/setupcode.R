015e39466f376b42d3249804a96b1b0261e3f8a9
Mark Neal <markbneal@gmail.com>
2019-12-12 15:52:44 +1300

    tested build, commented out non-essential commands

--- R/setupcode.R
+++ R/setupcode.R
@@ -1,16 +1,16 @@
-#Getting set up
-install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
-install.packages("rstudioapi")
-rstudioapi::isAvailable("0.99.149")
-devtools::install_github("r-lib/devtools") #not complete
-library(devtools)
-has_devel()
-#use in [Tools] [Shell]
-# git config --global user.name "Mark Neal"
-# git config --global user.email "markbneal@gmail.com"
-
-file.exists("~/.ssh/id_rsa.pub") #not here
-
-file.exists("c:/users/nealm/.ssh/id_rsa.pub") #is here
-
-#cahnge
+# #Getting set up
+# install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
+# install.packages("rstudioapi")
+# rstudioapi::isAvailable("0.99.149")
+# devtools::install_github("r-lib/devtools") #not complete
+# library(devtools)
+# has_devel()
+# #use in [Tools] [Shell]
+# # git config --global user.name "Mark Neal"
+# # git config --global user.email "markbneal@gmail.com"
+#
+# file.exists("~/.ssh/id_rsa.pub") #not here
+#
+# file.exists("c:/users/nealm/.ssh/id_rsa.pub") #is here
+#
+# #cahnge
